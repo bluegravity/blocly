bf89b98823c0ae9ec7be09f180e070c0d0053856
Dan <dlazarowski@yahoo.com>
2015-07-13 13:07:25 -0400

    38 - image library

--- app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
+++ app/src/main/java/io/bloc/android/blocly/BloclyApplication.java
@@ -2,6 +2,12 @@ package io.bloc.android.blocly;
 
 import android.app.Application;
 
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
 import io.bloc.android.blocly.api.DataSource;
 
 /**
@@ -28,10 +34,36 @@ public class BloclyApplication extends Application {
         super.onCreate();
         sharedInstance = this;
         dataSource = new DataSource();
+
+        // default will cache both in memory and on disk to reduce reload costs
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .build();
+
+        // Options for library. Mostly from README
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(this)
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+
+        ImageLoader.getInstance().init(configuration);
+
+
+
+
     }
 
     public DataSource getDataSource() {
         return dataSource;
     }
+
+
+
 
 }
